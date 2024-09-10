17e159c3d10e6e9bf6ffbf1c72d4a2847ccdc21f
ReevesA1 <adamrivard87@gmail.com>
2024-09-10 13:36:05 -0400

    better winget commands

--- Fresh/Data-Manager Windows Build/Bootstrap-Apps.ps1
+++ Fresh/Data-Manager Windows Build/Bootstrap-Apps.ps1
@@ -1,29 +1,44 @@
-# List of Winget applications to install
-$apps = @(
-    "Microsoft.PowerShell ",
-    "AppWork.JDownloader",
-    "Mozilla.Firefox",
-    "Python.Python.3.11",
-    "marktext",
-    "Microsoft.VisualStudioCode",
-    "Microsoft.PowerToys",
-    "Nvidia.GeForceExperience",
-    "DevToys-app.DevToys",
-    "VideoLAN.VLC",
-    "Notion.Notion",
-    "LocalSend.LocalSend",
-    "charmbracelet.gum",
-    "Git.Git",
-    "GitHub.cli",
-    "Docker.DockerDesktop"
-)
+# Admin
+winget install -e --id Microsoft.PowerShell
+winget install -e --id Python.Python.3.11
+winget install -e --id Microsoft.PowerToys
+winget install -e --id Nvidia.GeForceExperience
+winget install DevToys-app.DevToys #! Not in the default winget.run website
+winget install -e --id GitHub.GitHubDesktop
+winget install -e --id Docker.DockerDesktop
 
-# Loop through each app and install it
-foreach ($app in $apps) {
-    if ($app -eq "marktext" -or $app -eq "charmbracelet.gum" -or $app -eq "DevToys-app.DevToys") {
-        winget install $app --accept-package-agreements --accept-source-agreements
-    }
-    else {
-        winget install -e --id $app --accept-package-agreements --accept-source-agreements
-    }
-}
+
+# Browsers
+winget install -e --id Mozilla.Firefox
+winget install -e --id TorProject.TorBrowser
+
+# Media
+winget install -e --id qBittorrent.qBittorrent
+winget install -e --id AppWork.JDownloader
+winget install -e --id VideoLAN.VLC
+
+# Productivity
+winget install -e --id Notion.Notion
+winget install -e --id Obsidian.Obsidian
+winget install -e --id Microsoft.VisualStudioCode
+winget install marktext #! Not in the default winget.run website
+
+# Survival
+winget install -e --id Kiwix.KiwixJS
+
+# Security
+winget install -e --id Bitwarden.Bitwarden
+winget install -e --id ProtonTechnologies.ProtonVPN
+
+# Private Communication
+winget install -e --id OpenWhisperSystems.Signal
+winget install -e --id Oxen.Session
+winget install -e --id LocalSend.LocalSend
+winget install -e --id OnionShare.OnionShare
+
+
+# Terminal
+winget install charmbracelet.gum
+winget install charmbracelet.glow
+winget install -e --id Git.Git
+winget install -e --id GitHub.cli
